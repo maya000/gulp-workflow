0abcd4c1289bf636c5135bf7074b78fb485d7e7e
unknown <raij099@CTS2WSCJMM1VV1.cts.maricopa.gov>
2015-07-23 14:44:31 -0700

    Initial commit to gulp-workflow

---
+++ gulp-tasks/images.js
@@ -0,0 +1,20 @@
+﻿var gulp = require('gulp'),
+    cache = require('gulp-cache'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rename = require('gulp-rename'),
+    notify = require('gulp-notify');
+
+gulp.task('images', function() {
+    return gulp.src('src/images/**/*.{png,jpg,gif}')
+        .pipe(cache(imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('dist/assets/images'))
+        .pipe(notify({ message: 'Images task complete' }));
+});
+
